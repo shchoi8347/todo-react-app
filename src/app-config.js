7a4d23981d31d2cfa59b48127d2d1f4bc7cfe654
shchoi8347 <csh@duksung.ac.kr>
2023-05-29 16:13:23 +0900

    5/29 수업 완료

--- src/app-config.js
+++ src/app-config.js
@@ -3,9 +3,9 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://localhost:8080";
+   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://csh01-TodoBackend.us-west-2.elasticbeanstalk.com";
+  backendHost = "http://PROD-TODO-BACKEND4-12345678.us-west-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
