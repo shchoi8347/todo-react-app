a08ba7d01a05ef9e02d0a81d92b877b86c4e0910
csh <csh@duksung.ac.kr>
2024-03-28 14:56:17 +0900

    api-config.js updated

--- src/api-config.js
+++ src/api-config.js
@@ -2,13 +2,13 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-// if (hostname === "localhost") {
-//   backendHost = "http://localhost:8080";
-// } else {
-//   backendHost = "https://api.fsoftwareengineer.com";
-// }
+if (hostname === "localhost") {
+  backendHost = "http://localhost:8080";
+} else {
+  backendHost = "http://cshtodobe02-env.eba-bawsfgb2.ap-northeast-2.elasticbeanstalk.com";
+}
 
 // 백엔드를 AWS에 배포하지 않은 경우 이 주소를 사용할 것
-backendHost = "http://localhost:8080";
+//backendHost = "http://localhost:8080";
 
 export const API_BASE_URL = `${backendHost}`;
